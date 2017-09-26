ef06ed9d63ea2f4349d2d60e2ed08d797f8ebc17
José Ignacio Oyarzún Inalef <jioyarzuni@gmail.com>
2017-09-25 23:19:52 -0400

    first commit

---
+++ src/Calculadora/MainClass.java
@@ -0,0 +1,14 @@
+package Calculadora;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainClass {
+    public Pepe static {
+        System.out.println ("Mensaje a mostrar");
+    }
+
+}
